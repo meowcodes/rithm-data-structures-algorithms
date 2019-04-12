627859904182eccb6e3a247548b9346c3d0380d3
meowcodes <meowcodes@gmail.com>
2019-04-12 11:26:02 -0700

    write bubble and insertion sort

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,3 +1,17 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for(let i = 1; i < arr.length; i++){
+    let curr = arr[i];
+    for(let j = i-1; j >= 0; j--){
+      if(curr < arr[j]) {
+        arr[j+1] = arr[j];
+        arr[j] = curr;
+      } else {
+        arr[j+1] = curr;
+        break;
+      }
+    }
+  }
+  return arr;
+}
 
 module.exports = insertionSort;
