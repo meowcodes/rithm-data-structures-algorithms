9e1569a99e6d1cb8af981117d7c7e856b82517af
meowcodes <meowcodes@gmail.com>
2019-04-12 11:56:18 -0700

    write mergesort

--- sorting/merge.js
+++ sorting/merge.js
@@ -1,3 +1,33 @@
-function mergeSort() {}
+function merge(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let sorted = [];
 
-module.exports = mergeSort;
+  while(i < arr1.length && j < arr2.length){
+    if(arr1[i] < arr2[j]) {
+      sorted.push(arr1[i]);
+      i++;
+    }else {
+      sorted.push(arr2[j]);
+      j++;
+    }
+  }
+
+  sorted.push(...arr1.slice(i));
+  sorted.push(...arr2.slice(j));
+
+  return sorted;
+}
+
+function mergeSort(arr) {
+  // break array until length is 0 or 1;
+  if(arr.length <= 1) return arr;
+  let left = mergeSort(arr.slice(0, Math.floor(arr.length/2)));
+  let right = mergeSort(arr.slice(Math.floor(arr.length/2)));
+  // merge each little one;
+  let sorted = merge(left, right);
+  console.log("IN MS", left, right, sorted);
+  return sorted
+}
+
+module.exports = {merge, mergeSort};
