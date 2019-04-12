b64a9f8361d8746dc41a400e913ded77fa8e2f4e
meowcodes <meowcodes@gmail.com>
2019-04-12 12:02:54 -0700

    write selection sort

--- sorting/selection.js
+++ sorting/selection.js
@@ -1,3 +1,21 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+    let minIdx = i;
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[j] < min) {
+        min = arr[j];
+        minIdx = j;
+      }
+    }
+    if(minIdx !== i) {
+      // move curr val to min spot
+      arr[minIdx] = arr[i];
+      // update min spot
+      arr[i] = min;
+    }
+  }
+  return arr;
+}
 
 module.exports = selectionSort;
